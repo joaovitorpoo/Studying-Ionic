297c43e070082ead22c15a29919ef6792ffbd13d
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2020-05-23 01:48:58 -0300

    Page Amigos

---
+++ primeiroApp/src/app/amigos/amigos.page.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+export interface amigo {
+  nome: String;
+  idade: number;
+  nivelAmizade: number;
+}
+
+@Component({
+  selector: 'app-amigos',
+  templateUrl: './amigos.page.html',
+  styleUrls: ['./amigos.page.scss'],
+})
+
+export class AmigosPage implements OnInit {
+  amigos: amigo[] = [
+  {nome: "Joao Vitor", idade: 18, nivelAmizade: 5},
+  {nome: "Joao Pedro", idade: 20, nivelAmizade: 3},
+  {nome: "Vitor Lima", idade: 28, nivelAmizade: 3},
+  {nome: "Algusto Flyntom", idade: 17, nivelAmizade: 2},
+  {nome: "Vitor Batista", idade: 19, nivelAmizade: 1}
+  ];
+
+  constructor(public actionSheetController: ActionSheetController) { }
+
+  ngOnInit() {
+  }
+
+  deletar(user: amigo) {
+   let index = this.amigos.indexOf(user);
+   this.amigos.splice(index, 1);
+  }
+
+  async presentActionSheet(usuario: amigo) {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [{
+        text: 'Deletar',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.deletar(usuario);
+        }
+      }, {
+        text: 'Editar',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+}
--- primeiroApp/src/app/app.component.ts
+++ primeiroApp/src/app/app.component.ts
@@ -13,37 +13,21 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Amigos',
+      url: '/amigos',
+      icon: 'people'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'Viagens',
+      url: '/viagens',
+      icon: 'bus'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
